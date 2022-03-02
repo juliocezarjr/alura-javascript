d995584f890296d7d15d12268c608f9974905cf2
Julio Barreto <juliocezarjr@gmail.com>
2022-03-02 20:31:30 -0300

    first commit

---
+++ exercitandoJs.js
@@ -0,0 +1,7 @@
+var a = 10
+var b = 0
+console.log(a/b) // Infinit
+
+var a = 0
+var b = 0
+console.log(a/b) // NaN
---
+++ teste1.js
@@ -0,0 +1,11 @@
+let nomeDaPropriedad = "01 nome invalido"
+
+const pessoa = {
+    nome: "Júlio",
+    idade: 40,
+    telefone: 982067755
+}
+console.log(pessoa.nome)
+
+pessoa.nomeDaPropriedad = 0
+console.log(pessoa)
