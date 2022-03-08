140f4ea60caa8222e829586c3d70b69588fa30f5
Julio Barreto <juliocezarjr@gmail.com>
2022-03-08 05:19:32 -0300

    Ajustando o commit para continuar o curso

--- teste1.js
+++ teste1.js
@@ -1,11 +1,26 @@
-let nomeDaPropriedad = "01 nome invalido"
+idades = [30, 35, 28]
+nomes = ["Ana", "Juliana", "Leonardo"]
+faculdade = [false, true, true]
 
-const pessoa = {
-    nome: "Júlio",
-    idade: 40,
-    telefone: 982067755
+funcionarios = [nomes,idades,faculdade]
+
+console.log(funcionarios[0][0])
+console.log(funcionarios[1][0])
+
+const cliente = {
+    nome: "Jose",
+    idade: 33,
+    email: "jose@email.com",
+    telefones: ["+550033338888", "+550033334444"]
 }
-console.log(pessoa.nome)
 
-pessoa.nomeDaPropriedad = 0
-console.log(pessoa)
+cliente.animalEstimacao = [{
+    nome: "Kripto",
+    raça: "Cão",
+    vacinado: true
+}]
+cliente.animalEstimacao.push({
+    nome: "Lex",
+    raça: "Gato",
+    vacinado: false
+})
